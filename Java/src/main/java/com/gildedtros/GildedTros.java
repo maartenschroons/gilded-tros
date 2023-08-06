6b3e57e872d85e1cdd5b97d640fd9f36683de297
Maarten Schroons <maartenschroons@hotmail.com>
2023-08-06 12:04:09 +0200

    refactor GildedTros.java

--- Java/src/main/java/com/gildedtros/GildedTros.java
+++ Java/src/main/java/com/gildedtros/GildedTros.java
@@ -1,64 +1,92 @@
 package com.gildedtros;
 
+import java.util.Set;
+
 class GildedTros {
-    Item[] items;
+	Item[] items;
 
-    public GildedTros(Item[] items) {
-        this.items = items;
-    }
+	private final static String GOOD_WINE = "Good Wine";
+	private final static String BACKSTAGE_PASSES = "Backstage passes";
+	private final static Set<String> LEGENDARY_ITEMS = Set.of("B-DAWG Keychain");
+	private final static Integer MAX_ITEM_QUALITY = 50;
+	private final static Integer BACKSTAGE_PASSES_10DAY_INCREASE_AMOUNT = 2;
+	private final static Integer BACKSTAGE_PASSES_5DAY_INCREASE_AMOUNT = 3;
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Good Wine")
-                    && !items[i].name.equals("Backstage passes for Re:Factor")
-                    && !items[i].name.equals("Backstage passes for HAXX"))
-            {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("B-DAWG Keychain")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+	public GildedTros(Item[] items) {
+		this.items = items;
+	}
 
-                    if (items[i].name.equals("Backstage passes for Re:Factor") || items[i].name.equals("Backstage passes for HAXX") ) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+	public void updateQuality() {
+		for (int itemsIndex = 0; itemsIndex < items.length; itemsIndex++) {
+			if (!LEGENDARY_ITEMS.contains(items[itemsIndex].name)) {
+				handleNormalItem(itemsIndex);
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+				handleBackstagePass(itemsIndex);
 
-            if (!items[i].name.equals("B-DAWG Keychain")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+				handleGoodWine(itemsIndex);
+			}
+		}
+	}
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Good Wine")) {
-                    if (!items[i].name.equals("Backstage passes for Re:Factor") && !items[i].name.equals("Backstage passes for HAXX")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("B-DAWG Keychain")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
-    }
+	private void handleNormalItem(int itemsIndex) {
+		if (!items[itemsIndex].name.equals(GOOD_WINE) && !items[itemsIndex].name.contains(BACKSTAGE_PASSES)) {
+			decreaseItemSellIn(itemsIndex);
+
+			if (isExpired(itemsIndex)) {
+				decreaseItemQuality(itemsIndex, 2);
+			} else {
+				decreaseItemQuality(itemsIndex, 1);
+			}
+		}
+	}
+
+	private void handleBackstagePass(int itemsIndex) {
+		if (items[itemsIndex].name.contains(BACKSTAGE_PASSES)) {
+			decreaseItemSellIn(itemsIndex);
+			if (!isExpired(itemsIndex)) {
+				if (items[itemsIndex].sellIn > 5 && items[itemsIndex].sellIn < 11) {
+					increaseItemQuality(itemsIndex, BACKSTAGE_PASSES_10DAY_INCREASE_AMOUNT);
+				} else if (items[itemsIndex].sellIn < 6) {
+					increaseItemQuality(itemsIndex, BACKSTAGE_PASSES_5DAY_INCREASE_AMOUNT);
+				} else {
+					increaseItemQuality(itemsIndex, 1);
+				}
+
+			} else {
+				decreaseItemQuality(itemsIndex, items[itemsIndex].quality);
+			}
+		}
+	}
+
+	private void handleGoodWine(int itemsIndex) {
+		if (items[itemsIndex].name.equals(GOOD_WINE)) {
+			decreaseItemSellIn(itemsIndex);
+			increaseItemQuality(itemsIndex, 1);
+		}
+	}
+
+	private void decreaseItemQuality(int itemsIndex, int decreaseAmount) {
+		if (items[itemsIndex].quality > decreaseAmount) {
+			items[itemsIndex].quality = items[itemsIndex].quality - decreaseAmount;
+		} else {
+			items[itemsIndex].quality = 0;
+		}
+	}
+
+	private void increaseItemQuality(int itemsIndex, int increaseAmount) {
+		int newItemQuality = items[itemsIndex].quality + increaseAmount;
+		if (newItemQuality < MAX_ITEM_QUALITY) {
+			items[itemsIndex].quality = newItemQuality;
+		} else {
+			items[itemsIndex].quality = MAX_ITEM_QUALITY;
+		}
+	}
+
+	private boolean isExpired(int itemsIndex) {
+		return items[itemsIndex].sellIn < 0;
+	}
+
+	private void decreaseItemSellIn(int itemsIndex) {
+		items[itemsIndex].sellIn = items[itemsIndex].sellIn - 1;
+	}
 }
